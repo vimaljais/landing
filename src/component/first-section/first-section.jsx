3e3cae1f78537475dd94927304552cb14aec422f
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2021-04-13 14:33:16 +0530

    navbar fix

--- src/component/first-section/first-section.jsx
+++ src/component/first-section/first-section.jsx
@@ -1,27 +1,24 @@
-
-
-
-
 const FirstSection = () => {
-    return(
-        <div className="container-1">
-            <div className="logo">
-                Allies
-            </div>
-            <div className="header">
-                <div className="item">Why Allies?</div>
-                <div className="item">Features</div>
-                <div className="item">Pricing</div>
-                <div className="item">
-                    <img src="/assets/login.png" className="login"/>
-                </div>
-            </div>
-            <div className="app-image">
-                <img src="/assets/firstapp1.png" className="app-image1"/>
-                <img src="/assets/firstapp2.png" className="app-image2"/>
-            </div>
-            
+  return (
+    <div className="container-1">
+      <div className="headerset">
+        <div className="logoset">Allies</div>
+        <div className="item">Why Allies?</div>
+        <div className="item">Features</div>
+        <div className="item">Pricing</div>
+        <div className="item">
+          <img
+            style={{ paddingTop: "7px" }}
+            src="/assets/login.png"
+            className="login"
+          />
         </div>
-    )
-}
+      </div>
+      <div className="app-image">
+        <img src="/assets/firstapp1.png" className="app-image1" />
+        <img src="/assets/firstapp2.png" className="app-image2" />
+      </div>
+    </div>
+  );
+};
 export default FirstSection;
